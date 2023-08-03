70eff8a0c80cbc7d3becebc7f7c36b8a5a6801e1
sheritsh <sheritsh@student.21-school.ru>
2023-08-03 11:38:29 +0300

    feat: add vector tests

---
+++ src/tests/tests_entry.h
@@ -0,0 +1,11 @@
+#ifndef SRC_S21_CONTAINERS_1_SRC_TESTS_TESTS_ENTRY_H_
+#define SRC_S21_CONTAINERS_1_SRC_TESTS_TESTS_ENTRY_H_
+
+#include <gtest/gtest.h>
+
+#include "../s21_containersplus/array/s21_array.h"
+#include "../s21_containers/vector/s21_vector.h"
+
+
+
+#endif  // SRC_S21_CONTAINERS_1_SRC_TESTS_TESTS_ENTRY_H_
